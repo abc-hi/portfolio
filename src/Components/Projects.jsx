804dd00e7110cabb45a77b8db81262c61a1668fb
Revathi <revathimohancse@gmail.com>
2026-04-22 11:44:27 +0100

    font size for project comp is fixed

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
 
             <div className="margin" >
             <span >
-           <div style={{ fontSize: "22px" }}> <h5 className="skills-title" style={{color:"white",textAlign:"center"}}>Projects</h5></div> {/* Use class for styling */} <br />
+           <div style={{ fontSize: "24px" }}> <h5 className="skills-title" style={{color:"white",textAlign:"center"}}>Projects</h5></div> {/* Use class for styling */} <br />
            
              <div style={{ fontSize: "20px" }}><p className="skills-titlepara" style={{color:"white",textAlign:"center",marginLeft:"15rem",marginRight:"15rem"}}>
 "My projects demonstrate a strong foundation in building full-stack applications using MongoDB, Express, React, and Node.js. I have successfully implemented user authentication, data management, and responsive design principles to create seamless user experiences. By focusing on code organization, modularity, and scalability, I aim to deliver applications that are both efficient and maintainable. My work reflects my commitment to continuous learning and adapting to new technologies, ensuring that I stay updated with the latest trends and best practices in web development."</p>        
@@ -27,7 +27,7 @@ const Projects = () => {
 <img src="https://raw.githubusercontent.com/abc-hi/projectimages/main/mypjtitemcatelogImage.png
 " alt="Project Screenshot" />
 <div className="card-body">
-    <div style={{ fontSize: "22px" }}> <h5 className="card-title">ItemCatelog</h5></div>
+    <div style={{ fontSize: "24px" }}> <h5 className="card-title">ItemCatelog</h5></div>
     <div style={{ fontSize: "20px" }}><p className="card-text">It is a ecommerce full stack project and created with MERN stack.</p>
     <p className="card-text">Technologies used : javascript, reactjs, nodejs, mongoDB, CSS, expressjs, Bootstrap, tailwind CSS, HTML</p>
 
@@ -38,7 +38,7 @@ const Projects = () => {
 <div className="card" style={{width: "18rem", backgroundColor: "var(--bs-dark)", color: "#fff"}}>
   <img src="https://raw.githubusercontent.com/abc-hi/projectimages/main/jobportal.png" alt="Project Screenshot" />
   <div className="card-body">
-    <div style={{ fontSize: "22px" }}> <h5 className="card-title">Job Portal</h5></div>
+    <div style={{ fontSize: "24px" }}> <h5 className="card-title">Job Portal</h5></div>
     <div style={{ fontSize: "20px" }}><p className="card-text">It is a ecommerce full stack project and created with MERN stack.</p>
     <p className="card-text">Technologies used:javascript, reactjs, nodejs, mongoDB, CSS, expressjs, Bootstrap, HTML</p>
     <a href="https://revasjobportal.netlify.app/" className="btn btn-primary" style={{ backgroundColor: "#1b1b2a"}}>Link to Project</a>
@@ -50,7 +50,7 @@ const Projects = () => {
   <div className="card" style={{width: "18rem", backgroundColor: "var(--bs-dark)", color: "#fff"}}>
   <img src="https://raw.githubusercontent.com/abc-hi/projectimages/main/weatherappscreenshot.png" alt="Project Screenshot" />
   <div className="card-body">
-    <div style={{ fontSize: "22px" }}> <h5 className="card-title">WeatherApp</h5></div>
+    <div style={{ fontSize: "24px" }}> <h5 className="card-title">WeatherApp</h5></div>
    <div style={{ fontSize: "20px" }}><p className="card-text">   A responsive Weather App built as a beginner frontend developer project for learning and practice, based on a tutorial, with some of my own styling customizations.</p>
     <p className="card-text">Technologies used:  reactjs, javascript, tailwind CSS, HTML</p>
     <a href="https://revasweatherapplication.netlify.app" className="btn btn-primary" style={{ backgroundColor: "#1b1b2a"}}>Link to Project</a>
@@ -61,7 +61,7 @@ const Projects = () => {
   <div className="card" style={{width: "18rem", backgroundColor: "var(--bs-dark)", color: "#fff"}}>
   <img src="https://raw.githubusercontent.com/abc-hi/projectimages/main/chatbot_screenshot.png" alt="Project Screenshot" />
   <div className="card-body">
-    <div style={{ fontSize: "22px" }}> <h5 className="card-title">RAG Based career Assistant Chatbot</h5></div>
+    <div style={{ fontSize: "24px" }}> <h5 className="card-title">RAG Based career Assistant Chatbot</h5></div>
     <div style={{ fontSize: "20px" }}><p className="card-text"> Developed a RAG-based AI Career Assistant Chatbot using FAISS, embeddings, and LLM integration to suggest job roles based on user skills.</p>
     <p className="card-text"> Built with FastAPI and Streamlit, with backend exposed via ngrok for real-time connectivity and testing.</p>
     {/* <a href="https://revasweatherapplication.netlify.app" className="btn btn-primary" style={{ backgroundColor: "#1b1b2a"}}>Link to Project</a> */}
