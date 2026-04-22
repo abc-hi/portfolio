4c452d33e0f7b9c755313e25e1769f5946656523
Revathi <revathimohancse@gmail.com>
2026-04-22 13:22:03 +0100

    updated on navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,23 +4,27 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div>
-           <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-  <div class="container-fluid">
-    <Link to="/" class="navbar-brand" href="#" style={{fontSize:"24px"}}>Portfolio</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+           <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+  <div className="container-fluid">
+    <Link to="/" className="navbar-brand" href="#" style={{fontSize:"24px"}}>Portfolio</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav mx-auto" style={{fontSize:"24px"}}>
-        <Link to="/About" class="nav-link active" aria-current="page" href="#">About</Link>
+    {/* <div className="collapse navbar-collapse" id="navbarNavAltMarkup"> */}
+            <div className="collapse navbar-collapse" id="navbarNav">
 
-        <Link to="/Skills" class="nav-link active" aria-current="page" href="#">Skills</Link>
-                        <Link to="/WorkExperience" class="nav-link active" aria-current="page" href="#">Work Experience</Link>
-        <Link to="/Projects" class="nav-link active" aria-current="page" href="#">Projects</Link>
-   <Link to="/Certifications" class="nav-link active" aria-current="page" href="#">Certifications</Link>
-        <Link to="/Visionary" class="nav-link active" aria-current="page" href="#">Visionary</Link>
+      {/* <div className="navbar-nav mx-auto" style={{fontSize:"24px"}}> */}
+                <div className="navbar-nav mx-auto text-center gap-2 gap-lg-4">
 
-        <Link to="/Education" class="nav-link active" aria-current="page" href="#">Education</Link>
+        <Link to="/About" className="nav-link active" aria-current="page" href="#">About</Link>
+
+        <Link to="/Skills" className="nav-link active" aria-current="page" href="#">Skills</Link>
+                        <Link to="/WorkExperience" className="nav-link active" aria-current="page" href="#">Work Experience</Link>
+        <Link to="/Projects" className="nav-link active" aria-current="page" href="#">Projects</Link>
+   <Link to="/Certifications" className="nav-link active" aria-current="page" href="#">Certifications</Link>
+        <Link to="/Visionary" className="nav-link active" aria-current="page" href="#">Visionary</Link>
+
+        <Link to="/Education" className="nav-link active" aria-current="page" href="#">Education</Link>
       </div>
     </div>
   </div>
