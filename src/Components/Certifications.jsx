bff1809a2949b1d8b1eb54ff31891d70a562c262
Revathi <revathimohancse@gmail.com>
2026-04-22 12:47:18 +0100

    certifications updated

--- src/Components/Certifications.jsx
+++ src/Components/Certifications.jsx
@@ -7,13 +7,13 @@ const Certifications = () => {
     <div>
         <About/>
                         <div className="page-container" style={{ backgroundColor: 'var(--bs-dark)', padding: "20px" }}>
-                        <h5 className="skills-title" style={{color:"white",textAlign:"center"}}>Certifications</h5> {/* Use class for styling */}
+                        <h5 className="skills-title" style={{color:"white",textAlign:"center", fontSize:"24px"}}>Certifications</h5> {/* Use class for styling */}
 <br />
             <div className="card mx-auto" data-bs-theme="dark" style={{maxWidth:"38rem"}}>
            
   <div className="card-body">
     
-    <h5 className="card-title" style={{fontSize:"24px"}}>Certifications Done:</h5><br />
+    <h5 className="card-title">Certifications Done:</h5><br />
     <h5 className="card-subtitle mb-1 text-body-primary">MERN STACK at GUVI, India</h5><br />
 <h5 className="card-subtitle mb-1 text-body-primary">
   AI Catalyst Program – Skillnet, Ireland 
