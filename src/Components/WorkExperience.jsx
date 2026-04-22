bda36dbdbb634fdf6e39e2bfa9c125b26960994f
Revathi <revathimohancse@gmail.com>
2026-04-22 12:39:46 +0100

    content changed on work exp comp

--- src/Components/WorkExperience.jsx
+++ src/Components/WorkExperience.jsx
@@ -4,14 +4,14 @@ const WorkExperience = () => {
   return (
           <div>
                         <div className="page-container" style={{ backgroundColor: 'var(--bs-dark)', padding: "20px" }}>
-                        <h5 className="skills-title" style={{color:"white",textAlign:"center"}}>Work Experience</h5> {/* Use class for styling */}
+                        <h5 className="skills-title" style={{color:"white",textAlign:"center",fontSize:'24px'}}>Work Experience</h5> {/* Use class for styling */}
 <br />
             <div className="card mx-auto" data-bs-theme="dark" style={{maxWidth:"66rem"}}>
            
   <div className="card-body">
     
-    <h4 className="card-title">Parqwik | Frontend Developer Intern | 19 Feb 2025 – 19 May 2025</h4>
-    <p className="card-text"><ul>
+    <h4 className="card-title" style={{fontSize:"24px"}}>Parqwik | Frontend Developer Intern | 19 Feb 2025 – 19 May 2025</h4>
+    <p className="card-text"><ul style={{fontSize:"20px"}}>
       <li>I Gained hands-on exposure to Agile development workflow and sprint planning in a real project environment.</li>
       <li>Worked with React.js, JavaScript, HTML, CSS, and Tailwind CSS to implement frontend features.
 </li>
@@ -27,32 +27,37 @@ const WorkExperience = () => {
 </div><br />
 <div className="card mx-auto" data-bs-theme="dark" style={{maxWidth:"66rem"}}>
   <div className="card-body">
-    <h4 className="card-title">Wipro Technologies Project Engineer Chennai, India | 2011 - 2015</h4>
+    <h4 className="card-title" style={{fontSize:"24px"}}>Wipro Technologies Project Engineer Chennai, India | 2011 - 2015</h4>
     
-<h5 className="card-title">BP GIP Pen Test Project – Client: British Petroleum (BP)</h5>
-<p className="card-text">Key Achievements:
- <ul>
-  <li>Assisted in CMS and Change Request (CR) processes, contributing to faster request processing and improved project workflow.</li>
- <li>Ensured accurate documentation and timely updates, helping minimize delays in service delivery for BP.</li>
- <li>Supported project operations and knowledge transfer, enabling the team to maintain operational efficiency and meet deadlines.</li>
- <li> Successfully supported project execution for BP, contributing to efficient process delivery, improved operational workflows, and client satisfaction, helping Wipro retain the BP contract.
+<h5 className="card-title" style={{fontSize:"20px"}}>BP GIP Pen Test Project – Client: British Petroleum (BP)</h5>
+<p className="card-text" style={{fontSize:"20px"}}>Key Achievements:
+ <ul style={{fontSize:"20px"}}>
+  <li>Contributed to CMS and Change Request (CR) activities, helping streamline request handling and improve overall workflow efficiency.
 </li>
-<li> Supported improvements in operational performance and project outcomes (~10% improvement in turnaround time)
+ <li>Maintained accurate documentation with timely updates, reducing delays in service delivery for BP.
+</li>
+ <li>Supported day-to-day project operations and facilitated knowledge sharing, ensuring smooth execution and on-time delivery.
+</li>
+ <li> Played a key role in successful project execution for BP by improving process efficiency and supporting operational workflows, which contributed to client satisfaction and contract retention for Wipro.</li>
+<li> Helped achieve operational improvements, resulting in approximately 10% reduction in turnaround time.
 </li>
  
 </ul>
-<h5 className="card-title">CUP CAMS Project – Client: CUP CAMS</h5></p>
-<p className="card-text">Key Achievements:
-  <ul>
-    <li>Performed manual testing to verify data accuracy and folder availability, ensuring reliable and error-free deliverables to the client.<li>
-      <li> Reported inconsistencies and supported issue resolution, enhancing data integrity and quality assurance.
+<h5 className="card-title" style={{fontSize:"24px"}}>CUP CAMS Project – Client: CUP CAMS</h5></p>
+<p className="card-text" style={{fontSize:"20px"}}>Key Achievements:
+  <ul style={{fontSize:"20px"}}>
+    <li>Conducted manual testing to ensure data accuracy and verify folder availability, delivering reliable outputs to the client.
 </li>
-      </li> Assisted in project workflows, enabling timely delivery of outputs and supporting the client’s trust in project execution.
-</li>
+      <li> Identified and reported data inconsistencies, assisting in faster issue resolution and strengthening data quality assurance.
 
-<li> Detected and helped resolve ~25–30% of potential data discrepancies before release, improving data reliability for the client.
 </li>
-<li> Successfully performed manual testing for CUP CAMS, identifying and resolving key defects, enhancing data accuracy, and supporting timely, error-free deliverables, which improved client confidence and operational efficiency.
+      <li> Supported project workflows to ensure timely delivery and maintain client confidence in execution quality.</li>
+
+
+<li> Identified and helped resolve around 25–30% of potential data issues before release, improving overall data reliability.
+
+</li>
+<li> SActively contributed to manual testing efforts by detecting defects early, improving accuracy, and ensuring smooth and timely delivery, which enhanced client trust and operational performance.
 </li>
   </ul></p>
  
