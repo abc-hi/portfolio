80bef269e78c3c0b6d6f625db7ccd4c887123cd5
Revathi <revathimohancse@gmail.com>
2026-04-22 12:44:50 +0100

    navbar fixed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div>
            <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div class="container-fluid">
-    <Link to="/" class="navbar-brand" href="#">Portfolio</Link>
+    <Link to="/" class="navbar-brand" href="#" style={{fontSize:"24px"}}>Portfolio</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
